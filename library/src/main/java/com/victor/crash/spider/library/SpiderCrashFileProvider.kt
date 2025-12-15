3eab712e60303d1e5cbe6e25e561f25c006c3d69
zhanghaifeng <zhanghaifeng@taodaji.com.cn>
2025-12-15 08:47:47 +0800

    modify provider config

---
+++ library/src/main/java/com/victor/crash/spider/library/SpiderCrashFileProvider.kt
@@ -0,0 +1,17 @@
+package com.victor.crash.spider.library
+
+import androidx.core.content.FileProvider
+
+/*
+ * -----------------------------------------------------------------
+ * Copyright (C) 2025-2035, by Victor, All rights reserved.
+ * -----------------------------------------------------------------
+ * File: SpiderCrashProvider
+ * Author: Victor
+ * Date: 2025/12/15 8:44
+ * Description: 
+ * -----------------------------------------------------------------
+ */
+
+class SpiderCrashFileProvider: FileProvider() {
+}
